8d06f689a93d3cbbacb0d6d576937e975673b1dc
Wise Munene Mutuku <wisemunene99@gmail.com>
2025-09-09 10:47:32 +0300

    updated ongoing part 2

--- challenge 1.js
+++ challenge 1.js
@@ -1,19 +1,22 @@
 // student grade generator
-let studentMarks = prompt("Please enter student marks");
-// the user is prompted to enter the marks
-
-stsudentMarks= parseInt(studentMarks, 10); // prompt is usually string formatted hence we are convcerting it to an integer
-// if marks are above 100, below 0 or anything that is not a number the an alert message will be displayed
-if (isNaN(marks) || marks < 0 || marks > 100) {
-  alert(" Invalid input! Please enter a number between 0 and 100.");
-} else {
-  let grade;    // grade is undefined it is yet to be assigned values
+function getStudentMarks(marks){
   if (marks > 79 && marks<=100) {grade = "A";
   }else if (marks >= 60 && marks<=79){ grade = "B";
   }else if (marks >= 49 && marks <=60){ grade = "C";
   }else if (marks >= 40 && marks <=49){ grade = "D";
   }else{ grade = "E";}
+}
+ let studentMarks = prompt("Please enter student marks");
+// the user is prompted to enter the marks
+  
+stsudentMarks= parseInt(studentMarks, 10); // prompt is usually string formatted hence we are convcerting it to an integer
+// if marks are above 100, below 0 or anything that is not a number the an alert message will be displayed
 
+if (isNaN(marks) || marks < 0 || marks > 100) {
+  alert(" Invalid input! Please enter a number between 0 and 100.");
+} else {
+  // if the input enters satisfies the above condition the user will proceed to get the grade
+  let grade = getStudentMarks(marks)
   alert("✅ Grade: " + grade); // used to show the user what the grade is
 }
 
--- challenge 2.js
+++ challenge 2.js
@@ -1,8 +1,19 @@
 // SPEED DETECTOR PROGRAM
 function  checkCarSpeed(speed){
     const speedLimit= 70;
-    if( speed <= speedLimit){
+    kmAboveSpeedLimit= 5; // this when you go above the speed limit
+   // if sped is less than 70 it should display ok
+    if( speed < speedLimit + kmAboveSpeedLimit){
         return "Ok";
-    }else if(speed <= 75 && speed >70){
-        return 
-    }}
+    }
+    // what is happening below is to get our points we are taking the users inputed speed minus the speed limit,
+    //then if the speed is greater than 12 the license will be suspended otherwise your points will be displayed
+   
+    const demeritPoints= Math.floor(speed - speedLimit) / 5  // how we are going to calculate the demerit points
+if( demeritPoints >= 12){
+    return "License suspended";
+}else{
+    return demeritPoints;
+}
+}
+checkCarSpeed(150);
